68ce71b26db2c9b2fda23d0f969744575e09412f
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2016-05-22 15:46:00 +0900

    アセットのプリコンパイルキャンセルその6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,6 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.enabled = false
   config.assets.compile = false
 
   # Generate digests for assets URLs.
---
+++ lib/tasks/assets.rake
@@ -0,0 +1,7 @@
+# assets:precompileがいらないのでherokuでもしないようにする
+Rake::Task["assets:precompile"].clear
+   namespace :assets do
+     task 'precompile' do
+     puts "Not pre-compiling assets..."
+   end
+end
